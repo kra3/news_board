9b950fdb54c0629823005d05114e8b64905d73d4
Arun Karunagath (kra3) <the1.arun@gmail.com>
2016-09-28 01:14:48 +0530

    WIP: CRUD for news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,8 +1,41 @@
 class NewsController < ApplicationController
+  def index
+    @all_news = News.all
+  end
+
   def new
   end
 
+  def show
+    @news = News.find(params[:id])
+  end
+
   def create
-    render plain: params[:news].inspect
+    @news = News.new(news_params)
+
+    if @news.save
+      redirect_to @news
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @news = News.find(params[:id])
+  end
+
+  def update
+    @news = News.find(params[:id])
+
+    if @news.update(news_params)
+      redirect_to @news
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def news_params
+    params.require(:news).permit(:title, :content)
   end
 end
