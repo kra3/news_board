8484d45f650300babc7dfe28bcac86c533b3d242
Arun Karunagath (kra3) <the1.arun@gmail.com>
2016-09-29 23:31:51 +0530

    added comment deletion and extracted some partials out

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,12 @@ class CommentsController < ApplicationController
     redirect_to news_path(@news)
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to news_path(@comment.news)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
